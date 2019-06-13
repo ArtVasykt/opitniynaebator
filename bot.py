45b50577d1fe587457076665bc02173817788e72
ArtVasykt <razorykt@gmail.com>
2019-06-13 16:21:47 +0300

    j

--- bot.py
+++ bot.py
@@ -19,8 +19,10 @@ def on_chat_message(msg):
         file = BytesIO()
         bot.download_file(msg['photo'][0]['file_id'], file)
         file.seek(0)
-        face = Image.open(file)
-        draw(face, 'альби петушкевич', 12, 2000, 0)
+        #face = Image.open(file).resize(380, 380)
+        bot.sendPhoto('экз.png', file)
+        #result = draw(face, 'альби петушкевич', 12, 2000, 0)
+        #bot.sendPhoto(chat_id, ('хуй.png', result))
     print('Chat Message:', content_type, chat_type, chat_id)
 
 
